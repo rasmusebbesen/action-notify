bded849acc28260c08153d150c452ec6711d910e
Rasmus Ebbesen <re@nodes.dk>
2019-08-30 15:50:14 +0200

    use an env var from triggering repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -l
 
-echo "hello $1"
-echo "hello $2"
+echo "Triggered by $1"
 
 curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET https://api.lyrics.ovh/v1/Coldplay/Adventure+of+a+Lifetime
