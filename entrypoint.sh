19214880f38680aa36efa4bb43118148a5f771c9
Rasmus Ebbesen <re@nodes.dk>
2019-08-30 15:32:21 +0200

    use inputs :D

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
 
 echo "hello $1"
+echo "hello $2"
 
 curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET https://api.lyrics.ovh/v1/Coldplay/Adventure+of+a+Lifetime
