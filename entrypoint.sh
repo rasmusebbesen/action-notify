22024baab1cd727781219f8738f7a261a1c75a17
Rasmus Ebbesen <re@nodes.dk>
2019-08-30 13:17:21 +0200

    bump

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -l
 
 echo "hello $1"
-echo curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET https://api.lyrics.ovh/v1/Coldplay/Adventure+of+a+Lifetime
+
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET https://api.lyrics.ovh/v1/Coldplay/Adventure+of+a+Lifetime
