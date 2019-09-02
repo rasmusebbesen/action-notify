ee3627177e3b85057f6e78b7d26adb4d26eb510e
Rasmus Ebbesen <re@nodes.dk>
2019-09-02 11:18:40 +0200

    added request to trigger circle ci job

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,18 @@
 
 echo "Triggered by $1"
 
-curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET https://api.lyrics.ovh/v1/Coldplay/Adventure+of+a+Lifetime
+curl -X POST \
+  'https://circleci.com/api/v1.1/project/github/nodes-projects/documentation/tree/develop?circle-token=$2' \
+  -H 'Accept: application/json' \
+  -H 'Accept-Encoding: gzip, deflate' \
+  -H 'Cache-Control: no-cache' \
+  -H 'Connection: keep-alive' \
+  -H 'Content-Type: application/json' \
+  -H 'Host: circleci.com' \
+  -H 'cache-control: no-cache' \
+  -d '{
+	"build_parameters": {
+		"TRIGGERED_BY": "$1",
+		"CIRCLE_JOB": "build"
+	}
+}'
