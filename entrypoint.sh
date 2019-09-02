c840496215d7afc75bac46427f3102c98a35d844
Rasmus Ebbesen <re@nodes.dk>
2019-09-02 12:32:16 +0200

    bumping bumping

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ echo "Triggered by $1"
 url="https://circleci.com/api/v1.1/project/github/nodes-projects/documentation/tree/develop?circle-token=$2"
 
 curl -X POST \
-  '${url}' \
+  $url \
   -H 'Accept: application/json' \
   -H 'Accept-Encoding: gzip, deflate' \
   -H 'Cache-Control: no-cache' \
