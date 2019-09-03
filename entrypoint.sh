c17c70947b52770dc5b493b1caddc550d56bde33
Rasmus Ebbesen <re@nodes.dk>
2019-09-03 14:05:13 +0200

    try try try

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 echo "Triggered by $1"
 
 url="https://circleci.com/api/v1.1/project/github/nodes-projects/documentation/tree/develop?circle-token=$2"
+trigger=$1
 
 curl -X POST \
   $url \
@@ -15,7 +16,7 @@ curl -X POST \
   -H 'cache-control: no-cache' \
   -d '{
 	"build_parameters": {
-		"TRIGGERED_BY": $1,
+		"TRIGGERED_BY": "$trigger",
 		"CIRCLE_JOB": "build"
 	}
 }' -v
