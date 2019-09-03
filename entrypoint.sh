fdaf6881febf1809403f2148bddb7bedc8a975e9
Rasmus Ebbesen <re@nodes.dk>
2019-09-03 15:03:33 +0200

    slimmed it down

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,12 @@
 #!/bin/sh -l
 
-echo "Triggered by $1"
-
-url="https://circleci.com/api/v1.1/project/github/nodes-projects/documentation/tree/develop?circle-token=$2"
-trigger="rasmusebbesen/readme2"
-
-generate_post_data()
-{
-  return "{\"build_parameters\": {\"TRIGGERED_BY\": \"$trigger\",\"CIRCLE_JOB\": \"build\"}}"
-}
+trigger=$1
+key=$2
+url="https://circleci.com/api/v1.1/project/github/nodes-projects/documentation/tree/develop?circle-token=$key"
+payload="{\"build_parameters\": {\"TRIGGERED_BY\": \"$trigger\",\"CIRCLE_JOB\": \"build\"}}"
 
 echo $(generate_post_data)
+echo $url
 
 curl -X POST \
   $url \
@@ -21,4 +17,4 @@ curl -X POST \
   -H 'Content-Type: application/json' \
   -H 'Host: circleci.com' \
   -H 'cache-control: no-cache' \
-  -d "$(generate_post_data)" -vv
+  -d "$payload" -vv
