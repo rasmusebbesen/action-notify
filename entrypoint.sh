b2f9f6aa1628605ef3220624e2d334e734cc9627
Rasmus Ebbesen <re@nodes.dk>
2019-09-03 15:09:54 +0200

    removing output statements

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,6 @@ key=$2
 url="https://circleci.com/api/v1.1/project/github/nodes-projects/documentation/tree/develop?circle-token=$key"
 payload="{\"build_parameters\": {\"TRIGGERED_BY\": \"$trigger\",\"CIRCLE_JOB\": \"build\"}}"
 
-echo $(generate_post_data)
-echo $url
-
 curl -X POST \
   $url \
   -H 'Accept: application/json' \
@@ -17,4 +14,4 @@ curl -X POST \
   -H 'Content-Type: application/json' \
   -H 'Host: circleci.com' \
   -H 'cache-control: no-cache' \
-  -d "$payload" -vv
+  -d "$payload"
