4b141719eb4f700dd339909f8ce92d25f8191c91
Rasmus Ebbesen <re@nodes.dk>
2019-09-03 14:15:57 +0200

    post data is now generated by a method

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,18 @@ echo "Triggered by $1"
 url="https://circleci.com/api/v1.1/project/github/nodes-projects/documentation/tree/develop?circle-token=$2"
 trigger=$1
 
+generate_post_data()
+{
+  cat <<EOF
+{
+	"build_parameters": {
+		"TRIGGERED_BY": "$trigger",
+		"CIRCLE_JOB": "build"
+	}
+}
+EOF
+}
+
 curl -X POST \
   $url \
   -H 'Accept: application/json' \
@@ -14,9 +26,4 @@ curl -X POST \
   -H 'Content-Type: application/json' \
   -H 'Host: circleci.com' \
   -H 'cache-control: no-cache' \
-  -d '{
-	"build_parameters": {
-		"TRIGGERED_BY": "$trigger",
-		"CIRCLE_JOB": "build"
-	}
-}' -v
+  -data "$(generate_post_data)" -v
